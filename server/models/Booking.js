c980a94614752563af3a3cfae17e61b766c6aece
lakshmankolpe <lakshmankolpe912@gmail.com>
2024-07-16 10:41:40 +0530

    folder setup create

---
+++ server/models/Booking.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Schema,model} from "mongoose";
+
+const newBooking = new Schema({
+
+})
+
+export default Booking
---
+++ server/models/Flat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Schema,model} from "mongoose"
+
+function Flat() {
+  return (
+    <div>Flat</div>
+  )
+}
+
+export default Flat
