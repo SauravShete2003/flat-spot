70126de1f811b42a124c456e78ca5fcd53db1fde
lakshmankolpe <lakshmankolpe912@gmail.com>
2024-07-16 12:53:52 +0530

    intial commit

--- server/models/Owner.js
+++ server/models/Owner.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Schema, model } from "mongoose"
 const NewOwner = new Schema({
     fullname: {
--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Schema, model } from "mongoose"
 
 const NewPayment = new Schema({
